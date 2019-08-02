539ab847255c4e909e8b58639307508720b6b28c
JinLingChristopher <jinl1037@hotmail.com>
2019-08-02 20:36:14 +0800

    remvoe cmake-build-debug directory.

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/workspace/algorithm-cpp
-# Build directory: /mnt/d/workspace/algorithm-cpp/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test "test_directedgraph")
-set_tests_properties(Test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/workspace/algorithm-cpp/CMakeLists.txt;20;add_test;/mnt/d/workspace/algorithm-cpp/CMakeLists.txt;0;")
