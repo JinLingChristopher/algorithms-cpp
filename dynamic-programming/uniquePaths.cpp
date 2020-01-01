8fead13395bdd6b7df6807dca391408aac10776a
JinLingChristopher <jinl1037@hotmail.com>
2020-01-01 19:31:16 +0800

    Add a new file.

---
+++ dynamic-programming/uniquePaths.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int uniquePaths(int m, int n) {
+    vector<vector<int>> table(m, vector<int>(n, 0));
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == 0 || j == 0) {
+                table[i][j] = 1;
+            } else {
+                table[i][j] = table[i - 1][j] + table[i][j - 1];
+            }
+        }
+    }
+    return table[m - 1][n - 1];
+}
+
+int main() {
+    int m = 7, n = 3;
+    auto result = uniquePaths(m, n);
+
+    cout << result << endl;
+
+    return 0;
+}
