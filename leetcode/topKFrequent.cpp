77a8ff8af6923052a916ce0f15baf95cc0b571ca
JinLingChristopher <jinl1037@hotmail.com>
2020-01-04 12:06:30 +0800

    add new files.

--- leetcode/topKFrequent.cpp
+++ leetcode/topKFrequent.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
+#include <queue>
 using namespace std;
 
-vector<int> topKFrequent(const vector<int> &nums, int k) {
-    map<int, int> table;
-    for (auto item : nums) {
-        table[item] += 1;
+vector<int> topKFrequent(const vector<int>& nums, int k) {
+    unordered_map<int, int> count;
+    for (auto item: nums) {
+        ++count[item];
     }
-    for (auto iter = table.cbegin(); iter != table.cend(); ++iter) {
-        cout << iter->first << "->" << iter->second << endl;
+    using value_t = pair<int, int>;
+    auto cmp = [](const value_t& lhs, const value_t& rhs) {
+        return lhs.second > rhs.second;
+    };
+    priority_queue<value_t, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
+    for (auto iter = count.begin(); iter != count.end(); ++iter) {
+        pq.push(*iter);
+        if (pq.size() > k) {
+            pq.pop();
+        }
     }
-
-    return vector<int>();
+    vector<int> result;
+    while (!pq.empty()) {
+        result.push_back(pq.top().first);
+        pq.pop();
+    }
+    return result;
 }
 
 int main() {
     vector<int> nums{1, 1, 1, 2, 2, 3};
     int k = 2;
     auto result = topKFrequent(nums, k);
+    for (auto item: result) {
+        cout << item << " ";
+    }
+    cout << endl;
+
     return 0;
 }
