9589c6f836b9c38ae273353f8849eff7d786a735
JinLingChristoher <jinl1037@hotmail.com>
2021-04-14 17:49:59 +0800

    .

---
+++ leetcode/maxDepthAfterSplit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> maxDepthAfterSplit(string seq) {
+    vector<int> result;
+
+    return result;
+}
+
+int main() {
+    string seq{"(()())"};
+
+    return 0;
+}
